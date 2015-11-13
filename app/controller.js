9cfb7f84cf8a60e5501fc744c36ae27592d1a308
zakairobbins <zakairobbins@gmail.com>
2015-11-13 14:22:09 -0800

    fix conflict

--- app/controller.js
+++ app/controller.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
   console.log("hello");
-  $("body").on("click", function(){
+  $("button").on("click", function(){
     $(".text").html("change");
     console.log("it worked");
   });
