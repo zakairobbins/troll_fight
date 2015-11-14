7146451e2189275af3159371bb84f436e419af47
zakairobbins <zakairobbins@gmail.com>
2015-11-14 15:23:15 -0800

    create basic attack button function and connect action to model's state

--- app/controller.js
+++ app/controller.js
@@ -1,8 +1,27 @@
 $(document).ready(function(){
-  console.log("hello");
-  $("button").on("click", function(){
-    $(".text").html("change");
-    console.log("it worked");
-  });
 
 });
+
+
+function attackButton() {
+  $(".attack").on("click", function(){
+    buttonsOff();
+    $(".text").html(messages.heroAttack + attack(Hero, Troll));
+    setTimeout(function() {$(".text").html(messages.trollAttack + attack(Troll, Hero));}, 2000);
+    setTimeout(function() {$(".text").html("Choose an action");}, 4000);
+    setTimeout(function() {buttonsOn()}, 4010);
+  });
+}
+
+function buttonsOff() {
+  $("button").prop("disabled", true);
+}
+
+function buttonsOn() {
+  $("button").prop("disabled", false);
+}
+
+function timeout() {
+  console.log("timeout");
+  return
+}
