b51749c47f1f9fe1a56fa174a407e5908d13dd57
zakairobbins <zakairobbins@gmail.com>
2016-02-03 11:19:50 -0800

    create troll actions

--- app/controller.js
+++ app/controller.js
@@ -1,17 +1,23 @@
+  var hero = new Hero(100, 8, 3);
+  var troll = new Troll(100, 7, 2);
 $(document).ready(function(){
+  console.log(attack(troll, hero));
+  // console.log(troll.attack())
+
+
+  $(".attack").on("click", function(){
+    buttonsOff();
+    $(".text").html(messages.heroAttack + attack(hero, troll));
+    setTimeout(function() {$(".text").html(messages.trollAttack + attack(troll, hero));}, 2000);
+    setTimeout(function() {$(".text").html("Choose an action");}, 4000);
+    setTimeout(function() {buttonsOn()}, 4010);
+    console.log("ji")
+  });
+
 
 });
 
 
-function attackButton() {
-  $(".attack").on("click", function(){
-    buttonsOff();
-    $(".text").html(messages.heroAttack + attack(Hero, Troll));
-    setTimeout(function() {$(".text").html(messages.trollAttack + attack(Troll, Hero));}, 2000);
-    setTimeout(function() {$(".text").html("Choose an action");}, 4000);
-    setTimeout(function() {buttonsOn()}, 4010);
-  });
-}
 
 function buttonsOff() {
   $("button").prop("disabled", true);
--- app/model.js
+++ app/model.js
@@ -1,18 +1,60 @@
-var Hero = {
-  health: 100,
-  attack: 8,
-  potions: 3
+var Hero = function Hero(health, attack, potions){
+  this.name = "You";
+  this._health = health;
+  this._attack = attack;
+  this._potions = potions;
 }
 
-var Troll = {
-  health: 100,
-  attack: 7,
-  potions: 2
+Hero.prototype.health = function(){
+  return this._health;
+};
+Hero.prototype.attack = function(){
+  return this._attack;
+};
+Hero.prototype.potions = function(){
+  return this._potions;
+};
+
+var Troll = function Troll(health, attack, potions){
+  this.name = "The troll";
+  this._health = health;
+  this._attack = attack;
+  this._potions = potions;
 }
+
+Troll.prototype.health = function(){
+  return this._health;
+};
+Troll.prototype.attack = function(){
+  return this._attack;
+};
+Troll.prototype.potions = function(){
+  return this._potions;
+};
+
+// Troll.prototype.actions = function() {
+//   if (this.health > 50) {
+//     attack(self, hero);
+//   }
+//   else{
+//     usePotion(self);
+//   }
+
+// }
+
+// function usePotion(target) {
+//   if target.potions > 0{
+//     target.health = target.health + 20;
+//     return target.health;
+//   }
+//   else {
+//     return target.name + " has no potions."
+//   }
+// }
 
 function attack(attacker, target) {
-  target.health = target.health - attacker.attack
-  return attacker.attack + " points"
+  target._health = target.health() - attacker.attack()
+  return attacker.attack() + " points"
 }
 
 var test = "check";
